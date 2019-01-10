5abbe62c20142940623e52075f07f8e714a3e25a
Ben Rothman <brothman7000@gmail.com>
2019-01-10 21:20:01 +0100

    Set up create-react-app and basic carousel component

---
+++ carousel/src/Carousel.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Carousel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div className="carousel">
+            </div>
+        );
+    }
+}
+ 
+export default Carousel;
